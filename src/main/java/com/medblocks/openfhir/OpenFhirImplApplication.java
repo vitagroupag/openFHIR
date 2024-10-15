88832aba689c455af4ff80f9f3dbef2be3687947
gandrejc <gasper.andrejc>
2024-10-15 16:43:47 +0200

    Preventing mongodb from auto-initialization, as it should be initialized only if property is set to mongo

--- src/main/java/com/medblocks/openfhir/OpenFhirImplApplication.java
+++ src/main/java/com/medblocks/openfhir/OpenFhirImplApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class, MongoAutoConfiguration.class})
 @EnableScheduling
 @Slf4j
 public class OpenFhirImplApplication {
