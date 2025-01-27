5f92bec96768aab0dbc68f4a14c2532a391038a3
Gunavardhan Madineni <gunavardhan.madineni@karkinos.in>
2025-01-27 11:55:11 +0530

    changes of mapping irrespective of named path

--- src/main/java/com/medblocks/openfhir/OpenEhrRmWorker.java
+++ src/main/java/com/medblocks/openfhir/OpenEhrRmWorker.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static com.medblocks.openfhir.fc.FhirConnectConst.OPENEHR_TYPE_NONE;
