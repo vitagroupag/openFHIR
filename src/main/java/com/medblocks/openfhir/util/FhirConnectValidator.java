92af751867d64f39dbb56d8bf04b951befb68a4c
Gunavardhan Madineni <gunavardhan.madineni@karkinos.in>
2025-03-04 17:22:38 +0530

    updated validation of aqlPath while uploading mappings

--- src/main/java/com/medblocks/openfhir/util/FhirConnectValidator.java
+++ src/main/java/com/medblocks/openfhir/util/FhirConnectValidator.java
@@ -11,8 +11,13 @@ import com.medblocks.openfhir.fc.schema.model.FhirConnectModel;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.medblocks.openfhir.fc.schema.model.Manual;
+import com.medblocks.openfhir.fc.schema.model.ManualEntry;
+import com.medblocks.openfhir.fc.schema.model.Mapping;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
+import org.ehrbase.openehr.sdk.aql.dto.path.AqlObjectPath;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -66,9 +71,62 @@ public class FhirConnectValidator {
 //        }
 
         // todo: imeplement other validation (fhir path, openehr paths, ..)
-
-
+        if(modelMapper.getMappings()!= null){
+            validateMappings(modelMapper.getMappings(), errors);
+        }
         return errors;
+    }
+
+    public void validateMappings(List<Mapping> mappings, List<String> errors){
+        for(Mapping mapping: mappings){
+            String aqlPath = formatPath(mapping);
+            if(!aqlPath.isEmpty()) {
+                try {
+                    AqlObjectPath.parse(aqlPath);
+                } catch (Exception error) {
+                    errors.add(error.getMessage());
+                }
+                if (mapping.getManual() != null) {
+                    validateManualMappings(mapping.getManual(),aqlPath,errors);
+                }
+            }
+            if(mapping.getFollowedBy() != null){
+                validateMappings(mapping.getFollowedBy().getMappings(),errors);
+            }
+            if(mapping.getReference() != null){
+                validateMappings(mapping.getReference().getMappings(), errors);
+            }
+        }
+    }
+
+    public void validateManualMappings(List<Manual> manuals, String aqlPath, List<String> errors){
+        for (Manual manual : manuals) {
+            if(manual.getOpenehr() != null) {
+                for (ManualEntry manualEntry : manual.getOpenehr()) {
+                    String manualAqlPath = aqlPath + "/" + manualEntry.getPath();
+                    try {
+                        AqlObjectPath.parse(manualAqlPath);
+                    } catch (Exception error) {
+                        errors.add(error.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+    private String formatPath(Mapping mapping){
+        String aqlPath;
+        if(mapping.getWith()== null || mapping.getWith().getOpenehr() == null ) {
+            aqlPath = "";
+        }
+        else{
+            aqlPath = mapping.getWith().getOpenehr().replace("$archetype","").replace("$composition","").replace("$reference","");
+        }
+        if(aqlPath.startsWith("/")){
+            aqlPath = aqlPath.replaceFirst("/","");
+        }
+
+        return aqlPath;
     }
 
 }
